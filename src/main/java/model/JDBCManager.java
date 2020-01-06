45bc1d207b46faa6d4dc4473e9aaa97965fbc4e6
Alex Zvozdin <zvyozdin@gmail.com>
2020-01-06 18:31:20 +0200

    Add getTablesNames() method to JDBCManager class. Make methods non-static

--- src/main/java/model/JDBCManager.java
+++ src/main/java/model/JDBCManager.java
@@ -6,33 +6,39 @@ import java.util.List;
 
 public class JDBCManager {
 
-    private static Connection connection;
+    private Connection connection;
 
     public static void main(String[] args) throws SQLException {
-
+        JDBCManager jdbcManager = new JDBCManager();
 
         String database = "business";
         String user = "root";
         String password = "root";
-        connection = connect(database, user, password);
+        jdbcManager.connect(database, user, password);
+        jdbcManager.getTablesNames();
+    }
+
+    public List getTablesNames() throws SQLException {
         DatabaseMetaData data = connection.getMetaData();
         ResultSet tables = data.getTables(null, null, "%", null);
         List result = new ArrayList<String>();
         while (tables.next()) {
             result.add(tables.getString("TABLE_NAME"));
         }
-        System.out.println(result.toString());
-
+        return result;
     }
 
-    private static Connection connect(String database, String user, String password) {
+    public void connect(String database, String user, String password) {
         try {
             Class.forName("com.mysql.jdbc.Driver");
         } catch (ClassNotFoundException e) {
             throw new IllegalStateException("Cannot find the driver in the classpath!", e);
         }
         try {
-            return DriverManager.getConnection(
+            if (connection != null) {
+                connection.close();
+            }
+            connection = DriverManager.getConnection(
                     "jdbc:mysql://localhost:3306/" + database + "?autoReconnect=true&useSSL=false", user, password);
         } catch (SQLException e) {
             connection = null;
