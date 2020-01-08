8195d249533dece881688bd555089fe25c550559
Alex Zvozdin <zvyozdin@gmail.com>
2020-01-08 23:33:19 +0200

    Create package View and Console class for input and output

---
+++ src/main/java/view/Console.java
@@ -0,0 +1,16 @@
+package view;
+
+import java.util.Scanner;
+
+public class Console implements View {
+
+    @Override
+    public void write(String message) {
+        System.out.println(message);
+    }
+
+    @Override
+    public String read() {
+        return new Scanner(System.in).nextLine();
+    }
+}
---
+++ src/main/java/view/View.java
@@ -0,0 +1,8 @@
+package view;
+
+public interface View {
+
+    void write(String message);
+
+    String read();
+}
