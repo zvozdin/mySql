f525f6ff22054338da5a910ab16a1d404f259837
Alex Zvozdin <zvyozdin@gmail.com>
2020-01-09 01:31:02 +0200

    Added check on the number of parameters to connect. Error printing moved to a individual method

--- src/main/java/controller/MainController.java
+++ src/main/java/controller/MainController.java
@@ -25,23 +25,31 @@ public class MainController {
         view.write("Enter the Database name, Username and Password in the format: connect|database|user|password");
 
         while (true) {
-            String input = view.read();
-            String[] data = input.split("\\|");
-            String database = data[1];
-            String user = data[2];
-            String password = data[3];
             try {
+                String input = view.read();
+                String[] data = input.split("\\|");
+                if (data.length != 4) {
+                    throw new IllegalArgumentException("Invalid number of parameters. " +
+                            "Expected 4. You enter ==> " + data.length);
+                }
+                String database = data[1];
+                String user = data[2];
+                String password = data[3];
                 manager.connect(database, user, password);
                 break;
             } catch (Exception e) {
-                String message = "" + e.getMessage();
-                if (e.getCause() != null) {
-                    message += "\n" + e.getCause().getMessage();
-                }
-                view.write("Fail for a reason ==> " + message);
-                view.write("Try again...");
+                printError(e);
             }
         }
         view.write("Success!");
+    }
+
+    private void printError(Exception e) {
+        String message = "" + e.getMessage();
+        if (e.getCause() != null) {
+            message += "\n" + e.getCause().getMessage();
+        }
+        view.write("Fail for a reason ==> " + message);
+        view.write("Try again...");
     }
 }
