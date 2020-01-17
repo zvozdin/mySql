33ab584eadb9c7914c5c9187fae5860d392e1091
Alex Zvozdin <zvyozdin@gmail.com>
2020-01-17 13:03:55 +0200

    Added DeleteRow and Update commands

--- src/main/java/controller/command/DeleteRow.java
+++ src/main/java/controller/command/DeleteRow.java
@@ -1,10 +1,14 @@
 package controller.command;
 
+import model.DataSet;
 import model.DatabaseManager;
 import view.View;
 
+import java.util.List;
+
 public class DeleteRow implements Command {
 
+    private static final String COMMAND_SAMPLE = "delete|tableName|column|value";
     private DatabaseManager manager;
     private View view;
 
@@ -15,11 +19,41 @@ public class DeleteRow implements Command {
 
     @Override
     public boolean canProcess(String command) {
-        return false;
+        return command.startsWith("delete|");
     }
 
     @Override
     public void process(String command) {
+        String[] data = command.split("\\|");
+        String[] commandToDelete = COMMAND_SAMPLE.split("\\|");
+        if (data.length != commandToDelete.length) {
+            throw new IllegalArgumentException(String.format(
+                    "Invalid number of parameters separated by '|'. Expected %s. You enter ==> %s. " +
+                            "Use command 'delete|tableName|column|value'",
+                    commandToDelete.length, data.length));
+        }
 
+        DataSet input = new DataSet();
+        for (int index = 1; index < data.length / 2; index++) {
+            String columnName = data[index * 2];
+            String value = data[index * 2 + 1];
+            input.put(columnName, value);
+        }
+
+        String tableName = data[1];
+        // TODO check table already exists or catch exception in JDBC in deleteRow() method and wrap into RuntimeException
+        List<String> tables = manager.getTablesNames();
+        for (String table : tables) {
+            if (tableName.equals(table)) {
+                manager.deleteRow(tableName, input);
+                view.write(String.format("Record '%s' deleted.", input));
+                // print table with values
+                new Find(manager, view).printTableHeader(tableName);
+                new Find(manager, view).printValues(tableName);
+                return;
+            }
+        }
+        throw new IllegalArgumentException(String.format(
+                "Table '%s' doesn't exist.", tableName));
     }
 }
--- src/main/java/controller/command/Update.java
+++ src/main/java/controller/command/Update.java
@@ -1,10 +1,14 @@
 package controller.command;
 
+import model.DataSet;
 import model.DatabaseManager;
 import view.View;
 
+import java.util.List;
+
 public class Update implements Command {
-    
+
+    private static final String COMMAND_SAMPLE = "update|tableName|column|value";
     private DatabaseManager manager;
     private View view;
 
@@ -15,11 +19,48 @@ public class Update implements Command {
 
     @Override
     public boolean canProcess(String command) {
-        return false;
+        return command.startsWith("update|");
     }
 
     @Override
     public void process(String command) {
+        String[] data = command.split("\\|");
+        if (data.length % 2 != 0) {
+            throw new IllegalArgumentException(String.format(
+                    "Invalid number of parameters separated by '|'. Expected even count. You enter ==> %s. " +
+                            "Use command 'update|tableName|column1|value1|column2|value2|...|columnN|valueN'",
+                    data.length));
+        }
 
+        String[] commandToInsert = COMMAND_SAMPLE.split("\\|");
+        if (data.length < commandToInsert.length) {
+            throw new IllegalArgumentException(String.format(
+                    "Invalid number of parameters separated by '|'. Expected min %s. You enter ==> %s. " +
+                            "Use command 'update|tableName|column1|value1|column2|value2|...|columnN|valueN'",
+                    commandToInsert.length, data.length));
+        }
+
+        DataSet input = new DataSet();
+        for (int index = 1; index < data.length / 2; index++) {
+            String columnName = data[index * 2];
+            String value = data[index * 2 + 1];
+            input.put(columnName, value);
+        }
+
+        String tableName = data[1];
+        // TODO check table already exists or catch exception in JDBC in insert() method and wrap into RuntimeException
+        List<String> tables = manager.getTablesNames();
+        for (String table : tables) {
+            if (tableName.equals(table)) {
+                manager.insert(tableName, input);
+                view.write(String.format("Record '%s' updated.", input));
+                // print table with values
+                new Find(manager, view).printTableHeader(tableName);
+                new Find(manager, view).printValues(tableName);
+                return;
+            }
+        }
+        throw new IllegalArgumentException(String.format(
+                "Table '%s' doesn't exist.", tableName));
     }
 }
