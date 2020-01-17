0a9ed78d40e58cebee3aa0f3e734bfc3a203e152
Alex Zvozdin <zvyozdin@gmail.com>
2020-01-17 15:42:50 +0200

    Refactored Help Command: added other commands

--- src/main/java/controller/command/Help.java
+++ src/main/java/controller/command/Help.java
@@ -23,25 +23,37 @@ public class Help implements Command {
         view.write("\t\tto display a list of commands");
 
         view.write("\tconnect|databaseName|user|password");
-        view.write("\t\tto connect to database");
+        view.write("\t\tto connect to the database");
 
         view.write("\tnewDatabase|databaseName");
-        view.write("\t\tto create new database");
+        view.write("\t\tto create a new database");
 
         view.write("\tdropDatabase|databaseName");
-        view.write("\t\tto delete database");
+        view.write("\t\tto delete the database");
 
         view.write("\tlist");
         view.write("\t\tto display a list of tables");
 
+        view.write("\tcreate|tableName|column1|column2|...|columnN");
+        view.write("\t\tto create a new table");
+
+        view.write("\tdrop|tableName");
+        view.write("\t\tto delete the table");
+
         view.write("\tfind|tableName");
         view.write("\t\tto retrieve content from the 'tableName'");
 
-        view.write("\tclear|tableName");
-        view.write("\t\tto delete content from the 'tableName'");
-
         view.write("\tinsert|tableName|column1|value1|column2|value2|...|columnN|valueN");
         view.write("\t\tto record content to the 'tableName'");
+
+        view.write("\tupdate|tableName|column1|value1|column2|value2|...|columnN|valueN");
+        view.write("\t\tto record to update the content in the 'tableName'");
+
+        view.write("\tdelete|tableName|column|value");
+        view.write("\t\tto delete content where column = value");
+
+        view.write("\tclear|tableName");
+        view.write("\t\tto delete content from the 'tableName'");
 
         view.write("\texit");
         view.write("\t\tto exit from the program");
