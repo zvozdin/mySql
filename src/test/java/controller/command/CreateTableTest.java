1a0b77cd6b27662687e4602dbff674464b3ffbb9
Alex Zvozdin <zvyozdin@gmail.com>
2020-01-19 18:09:09 +0200

    Writed tests for CreateTable Command

---
+++ src/test/java/controller/command/CreateTableTest.java
@@ -0,0 +1,63 @@
+package controller.command;
+
+import model.DataSet;
+import model.DatabaseManager;
+import org.junit.Before;
+import org.junit.Test;
+import view.View;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+public class CreateTableTest {
+
+    private Command command;
+    private DatabaseManager manager;
+    private View view;
+
+    @Before
+    public void setup() {
+        manager = mock(DatabaseManager.class);
+        view = mock(View.class);
+        command = new CreateTable(manager, view);
+    }
+
+    @Test
+    public void testCanProcess_CorrectCreateTableCommand() {
+        assertTrue(command.canProcess("create|test|id"));
+    }
+
+    @Test
+    public void testCanProcess_WrongCreateTableCommand() {
+        assertFalse(command.canProcess("create"));
+    }
+
+    @Test
+    public void testProcess_CreateTable() {
+        // given
+        DataSet input = new DataSet();
+        input.put("id", "");
+
+        // when
+        command.process("create|test|id");
+
+        // then
+        verify(manager, atMostOnce()).createTable("test", input);
+        verify(view).write("Table 'test' created.");
+    }
+
+    @Test
+    public void testProcess_CreateDatabaseCommandWithWrongParameters() {
+        // when
+        try {
+            command.process("create|test|");
+            fail("Expected IllegalArgumentException");
+        } catch (Exception e) {
+            // then
+            assertEquals("" +
+                    "Invalid number of parameters separated by '|'.\n" +
+                    "Expected no less than 3. You enter ==> 2.\n" +
+                    "Use command 'create|tableName|columnName'", e.getMessage());
+        }
+    }
+}
