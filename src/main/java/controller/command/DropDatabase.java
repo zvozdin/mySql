da7dea9d29e0727651ecea7e05d287e6c41c6281
Alex Zvozdin <zvyozdin@gmail.com>
2020-01-19 20:58:40 +0200

    Wrote tests for DropDatabase Command

--- src/main/java/controller/command/DropDatabase.java
+++ src/main/java/controller/command/DropDatabase.java
@@ -22,14 +22,10 @@ public class DropDatabase implements Command {
     @Override
     public void process(String command) {
         parametersNumberValidation(COMMAND_SAMPLE, command);
+
         String[] data = command.split("\\|");
-
-//        String[] commandToDrop = COMMAND_SAMPLE.split("\\|");
-//        if (data.length != commandToDrop.length) {
-//            throw new InvalidParametersNumberException(commandToDrop.length, data.length, COMMAND_SAMPLE);
-//        }
-
         String databaseName = data[1];
+
         manager.dropDatabase(databaseName);
         view.write(String.format("Database '%s' deleted.", databaseName));
     }
