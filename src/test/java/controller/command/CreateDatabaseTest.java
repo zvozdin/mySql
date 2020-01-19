dbf5238a4d1a628e416bc60e8fce68f8e77f28f9
Alex Zvozdin <zvyozdin@gmail.com>
2020-01-19 15:18:32 +0200

    Tested CreateDatabase Command

---
+++ src/test/java/controller/command/CreateDatabaseTest.java
@@ -0,0 +1,59 @@
+package controller.command;
+
+import model.DatabaseManager;
+import org.junit.Before;
+import org.junit.Test;
+import view.View;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+public class CreateDatabaseTest {
+
+    private Command command;
+    private DatabaseManager manager;
+    private View view;
+
+    @Before
+    public void setup() {
+        manager = mock(DatabaseManager.class);
+        view = mock(View.class);
+        command = new CreateDatabase(manager, view);
+    }
+
+    @Test
+    public void testCanProcess_CorrectFindCommand() {
+        assertTrue(command.canProcess("newDatabase|databaseName"));
+    }
+
+    @Test
+    public void testCanProcess_WrongFindCommand() {
+        assertFalse(command.canProcess("newDatabase"));
+    }
+
+    @Test
+    public void testProcess_CreateDatabase() {
+        // when
+        command.process("newDatabase|databaseName");
+
+        // then
+        verify(manager).createDatabase("databaseName");
+        verify(view).write("Database 'databaseName' created.");
+    }
+
+    @Test
+    public void testProcess_CreateDatabaseCommandWithWrongParameters() {
+        // when
+        try {
+            command.process("newDatabase|");
+            fail("Expected InvalidParametersNumberException");
+        } catch (Exception e) {
+            // then
+            assertEquals("" +
+                    "Invalid parameters number separated by '|'.\n" +
+                    "Expected 2. You enter ==> 1.\n" +
+                    "Use command 'newDatabase|databaseName'", e.getMessage());
+        }
+    }
+}
