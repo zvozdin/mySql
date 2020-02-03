e6c54ddad6bc20b01a88bdd1c28db1bf340057f2
Alex Zvozdin <zvyozdin@gmail.com>
2020-02-03 18:17:22 +0200

    refactoring: renamed variables

--- src/main/java/ua/com/juja/view/TableGenerator.java
+++ src/main/java/ua/com/juja/view/TableGenerator.java
@@ -8,32 +8,32 @@ public class TableGenerator {
 
     static final int PADDINGS = 2;
     private static final String NEW_LINE = "\n";
-    private static final String TABLE_JOINT_SYMBOL = "+";
-    private static final String TABLE_SPLIT_SYMBOL = "|";
-    private static final String TABLE_MINUS_SYMBOL = "-";
+    private static final String PLUS = "+";
+    private static final String SPLIT = "|";
+    private static final String MINUS = "-";
 
     public String generateTable(List<String> columns, List<List<String>> rows) {
-        StringBuilder stringBuilder = new StringBuilder();
+        StringBuilder table = new StringBuilder();
 
         Map<Integer, Integer> columnsNumberAndSize = getTableWidth(columns, rows);
 
-        createRowLine(stringBuilder, columns, columnsNumberAndSize);
-        stringBuilder.append(NEW_LINE);
+        appendLine(table, columns, columnsNumberAndSize);
+        table.append(NEW_LINE);
 
-        putData(stringBuilder, columns, columnsNumberAndSize);
-        stringBuilder.append(NEW_LINE);
+        putData(table, columns, columnsNumberAndSize);
+        table.append(NEW_LINE);
 
-        createRowLine(stringBuilder, columns, columnsNumberAndSize);
-        stringBuilder.append(NEW_LINE);
+        appendLine(table, columns, columnsNumberAndSize);
+        table.append(NEW_LINE);
 
-        for (List<String> row : rows) { // TODO test putDATA for value + 2 rows + whole table
-            putData(stringBuilder, row, columnsNumberAndSize);
-            stringBuilder.append(NEW_LINE);
+        for (List<String> row : rows) {
+            putData(table, row, columnsNumberAndSize);
+            table.append(NEW_LINE);
         }
 
-        createRowLine(stringBuilder, columns, columnsNumberAndSize);
+        appendLine(table, columns, columnsNumberAndSize);
 
-        return stringBuilder.toString();
+        return table.toString();
     }
 
     Map<Integer, Integer> getTableWidth(List<String> columns, List<List<String>> rows) {
@@ -72,49 +72,45 @@ public class TableGenerator {
         }
     }
 
-    void createRowLine(StringBuilder stringBuilder, List<String> columns, Map<Integer, Integer> columnsNumberAndSize) {
+    void appendLine(StringBuilder table, List<String> columns, Map<Integer, Integer> columnsNumberAndSize) {
         for (int index = 0; index < columns.size(); index++) {
             if (index == 0) {
-                stringBuilder.append(TABLE_JOINT_SYMBOL);
+                table.append(PLUS);
             }
 
             Integer oneColumnWidth = columnsNumberAndSize.get(index);
             for (int j = 0; j < oneColumnWidth + PADDINGS * 2; j++) {
-                stringBuilder.append(TABLE_MINUS_SYMBOL);
+                table.append(MINUS);
             }
 
-            stringBuilder.append(TABLE_JOINT_SYMBOL);
+            table.append(PLUS);
         }
     }
 
-    void putData(StringBuilder stringBuilder, List<String> data, Map<Integer, Integer> columnsNumberAndSize) {
+    void putData(StringBuilder table, List<String> data, Map<Integer, Integer> columnsNumberAndSize) {
         for (int index = 0; index < data.size(); index++) {
             String value = data.get(index);
-            fillColumn(stringBuilder, value, index, columnsNumberAndSize);
+            fillColumn(table, value, index, columnsNumberAndSize);
         }
     }
 
-    void fillColumn(
-            StringBuilder stringBuilder, String value, int index, Map<Integer, Integer> columnsNumberAndSize)
-    {
+    void fillColumn(StringBuilder table, String value, int index, Map<Integer, Integer> columnsNumberAndSize) {
         int paddingSize = getOptimumPaddingSize(index, value.length(), columnsNumberAndSize, PADDINGS);
         if (index == 0) {
-            stringBuilder.append(TABLE_SPLIT_SYMBOL);
+            table.append(SPLIT);
         }
 
-        fillSpace(stringBuilder, paddingSize);
-        stringBuilder.append(value);
+        fillSpace(table, paddingSize);
+        table.append(value);
         if (value.length() % 2 != 0) {
-            stringBuilder.append(" ");
+            table.append(" ");
         }
 
-        fillSpace(stringBuilder, paddingSize);
-        stringBuilder.append(TABLE_SPLIT_SYMBOL);
+        fillSpace(table, paddingSize);
+        table.append(SPLIT);
     }
 
-    int getOptimumPaddingSize(
-            int index, int valueLength, Map<Integer, Integer> columnsNumberAndSize, int paddings)
-    {
+    int getOptimumPaddingSize(int index, int valueLength, Map<Integer, Integer> columnsNumberAndSize, int paddings) {
         if (valueLength % 2 != 0) {
             valueLength++;
         }
@@ -126,9 +122,9 @@ public class TableGenerator {
         return paddings;
     }
 
-    private void fillSpace(StringBuilder stringBuilder, int length) {
+    private void fillSpace(StringBuilder table, int length) {
         for (int i = 0; i < length; i++) {
-            stringBuilder.append(" ");
+            table.append(" ");
         }
     }
 }
