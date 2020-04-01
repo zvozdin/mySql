e77295e34d38a94dac34019e278c826b1d2a4435
Alex Zvozdin <zvyozdin@gmail.com>
2020-04-02 00:32:25 +0300

    refactoring: delete web.xml and added SpringConfig

---
+++ src/main/java/ua/com/juja/config/SpringConfig.java
@@ -0,0 +1,26 @@
+package ua.com.juja.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+@Configuration
+@ComponentScan("ua.com.juja")
+@EnableWebMvc
+public class SpringConfig implements WebMvcConfigurer {
+
+    @Bean
+    public ViewResolver viewResolver() {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setViewClass(JstlView.class);
+        resolver.setPrefix("/view/");
+        resolver.setSuffix(".jsp");
+
+        return resolver;
+    }
+}
