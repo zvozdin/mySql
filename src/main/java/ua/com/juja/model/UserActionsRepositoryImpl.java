9f32fa7934fc9506d77d2ad6c232408f60b08323
Alex Zvozdin <zvyozdin@gmail.com>
2020-04-18 00:16:24 +0300

    refactoring: added hibernate entityManager

--- src/main/java/ua/com/juja/model/UserActionsRepositoryImpl.java
+++ src/main/java/ua/com/juja/model/UserActionsRepositoryImpl.java
@@ -1,23 +1,29 @@
 package ua.com.juja.model;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 import ua.com.juja.model.entity.DatabaseConnection;
 import ua.com.juja.model.entity.UserAction;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
 public class UserActionsRepositoryImpl implements UserActionsRepositoryCustom {
 
-    @Autowired
-    private UserActionsRepository userActions;
+    @PersistenceContext
+    private EntityManager entityManager;
 
     @Autowired
     private DatabaseConnectionsRepository databaseConnections;
 
     @Override
+    @Transactional(propagation = Propagation.REQUIRED )
     public void saveAction(String action, String user, String database) {
         DatabaseConnection databaseConnection = databaseConnections.findByUserNameAndDbName(user, database);
         if (databaseConnection == null) {
             databaseConnection = databaseConnections.save(new DatabaseConnection(user, database));
         }
-        userActions.save(new UserAction(action, databaseConnection));
+        entityManager.persist(new UserAction(action, databaseConnection));
     }
 }
