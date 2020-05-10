d3b1b079b842bd342ca1cfa508768093687381ad
Alex Zvozdin <zvyozdin@gmail.com>
2020-05-10 10:02:28 +0300

    refactored: extracted HibernateConfig and imported to Config

--- src/main/java/ua/com/juja/config/SpringConfig.java
+++ src/main/java/ua/com/juja/config/SpringConfig.java
@@ -1,16 +1,9 @@
 package ua.com.juja.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
-import org.springframework.orm.jpa.JpaTransactionManager;
-import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
-import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
-import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.context.annotation.Import;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
@@ -18,28 +11,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
 
-import javax.persistence.EntityManagerFactory;
-import javax.sql.DataSource;
-import java.util.Properties;
-
 @Configuration
 @EnableWebMvc
-@EnableJpaRepositories("ua.com.juja")
 @ComponentScan("ua.com.juja")
-@PropertySource("classpath:log.jdbc.properties")
+@Import(HibernateConfig.class)
 public class SpringConfig implements WebMvcConfigurer {
-
-    @Value("${log.jdbc.driverClassName}")
-    private String driverClassName;
-
-    @Value("${log.jdbc.url}")
-    private String url;
-
-    @Value("${log.jdbc.username}")
-    private String userName;
-
-    @Value("${log.jdbc.password}")
-    private String password;
 
     @Override
     public void addResourceHandlers(final ResourceHandlerRegistry registry) {
@@ -54,45 +30,5 @@ public class SpringConfig implements WebMvcConfigurer {
         resolver.setSuffix(".jsp");
 
         return resolver;
-    }
-
-    @Bean
-    public DataSource dataSource() {
-        DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName(driverClassName);
-        dataSource.setUrl(url);
-        dataSource.setUsername(userName);
-        dataSource.setPassword(password);
-
-        return dataSource;
-    }
-
-    @Bean
-    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
-        HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
-//        vendorAdapter.setGenerateDdl(false);
-//        vendorAdapter.setShowSql(true);
-
-        LocalContainerEntityManagerFactoryBean factory = new LocalContainerEntityManagerFactoryBean();
-        factory.setJpaVendorAdapter(vendorAdapter);
-        factory.setJpaProperties(additionalProperties());
-        factory.setPackagesToScan("ua.com.juja");
-        factory.setDataSource(dataSource());
-        return factory;
-    }
-
-    @Bean
-    public PlatformTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
-        JpaTransactionManager txManager = new JpaTransactionManager();
-        txManager.setEntityManagerFactory(entityManagerFactory);
-        return txManager;
-    }
-
-    Properties additionalProperties() {
-        Properties properties = new Properties();
-        properties.setProperty("hibernate.hbm2ddl.auto", "validate");
-        properties.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQLDialect");
-        properties.setProperty("hibernate.show_sql", "true");
-        return properties;
     }
 }
